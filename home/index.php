fd35b690a60f14f08ee28d0f0a16b826d06d5a10
lucashd <lhdavid14@gmail.com>
2014-02-08 18:02:39 -0800

    Added Julie's bootstrap nav bar

--- home/index.php
+++ home/index.php
@@ -6,6 +6,7 @@
 <html>
 <head>
     <title>Home</title>
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="../css/smoothness/jquery-ui-1.10.3.custom.min.css">
     <link rel="stylesheet" type="text/css" href="../css/style.css">
     <script src="../js/parse.min.js"></script>
@@ -14,11 +15,14 @@
 
     <script src="../js/highcharts/js/highcharts.js"></script>
     <script src="../js/highcharts/js/modules/exporting.js"></script>
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
     <script src="../js/scripts.js"></script>
+
 
 
 </head>
 <body>
+<!--
 <nav>
     <ul>
         <li>
@@ -38,6 +42,40 @@
         </li>
     </ul>
 
+</nav>
+!-->
+<nav class="navbar navbar-default" role="navigation">
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Panhellenic <b class="caret"></b></a>
+                <ul class="dropdown-menu" id="panhellenic">
+                    <li><a href="#">Kappa Alpha Theta</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">IFC <b class="caret"></b></a>
+                <ul class="dropdown-menu" id="ifc">
+                    <li><a href="#">Phi Sig</a></li>
+                </ul>
+            </li>
+            <li><a href="#"><img id="logo" src="../img/logo.png"></a></li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">USFC <b class="caret"></b></a>
+                <ul class="dropdown-menu" id="usfc">
+                    <li><a href="#">Chi Delta Theta</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Sports <b class="caret"></b></a>
+                <ul class="dropdown-menu" id="sports">
+                    <li><a href="#">Men's Soccer</a></li>
+                </ul>
+            </li>
+
+        </ul>
+    </div><!-- /.navbar-collapse -->
 </nav>
 
 <div id="container">
